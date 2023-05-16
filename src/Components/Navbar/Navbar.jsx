bc2a12404238bd293f7aa912d21bbb93e9d2b943
taiwo-art <lmb.taiwo@gmail.com>
2023-05-16 17:41:02 +0100

    add linkedin,github and resume links. Also fixed view more work button

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
-import Logo from "../../Assets/Logo.jpg";
-import { FaBars, FaTimes,FaGithub, FaFacebook, FaLinkedin } from "react-icons/fa";
-import { HiOutlineMail } from "react-icons/hi"; 
-import {BsFillPersonLinesFill} from 'react-icons/bs';
-import { Link } from "react-scroll";
+import React, { useState } from 'react';
+import Logo from '../../Assets/Logo.jpg';
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
   const [navbars, setNavbars] = useState(false);
@@ -11,25 +10,35 @@ const Navbar = () => {
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-slate-900 text-gray-300">
       <div>
-        <img src={Logo} alt="Taiwo's Logo" style={{ width: "50px" }} />
+        <img src={Logo} alt="Taiwo's Logo" style={{ width: '50px' }} />
       </div>
       {/* menu */}
       <ul className="hidden md:flex">
-        <li><Link to="home" smooth={true} duration={500}>
-          Home
-        </Link></li>
-        <li><Link to="about" smooth={true} duration={500}>
-          About
-        </Link></li>
-        <li><Link to="skills" smooth={true} duration={500}>
-          Skills
-        </Link></li>
-        <li><Link to="work" smooth={true} duration={500}>
-          Work
-        </Link></li>
-        <li><Link to="contact" smooth={true} duration={500}>
-          Contact
-        </Link></li>
+        <li>
+          <Link to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link to="work" smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li>
+          <Link to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
       </ul>
       {/* hamburger menu */}
       <div onClick={handleClick} className="md:hidden z-10">
@@ -41,34 +50,68 @@ const Navbar = () => {
       <ul
         className={
           !navbars
-            ? "hidden"
-            : "absolute top-0 left-0 w-full h-screen bg-slate-900 flex flex-col justify-center items-center"
+            ? 'hidden'
+            : 'absolute top-0 left-0 w-full h-screen bg-slate-900 flex flex-col justify-center items-center'
         }
       >
-        <li className="py-6 text-4xl"><Link onClick={handleClick} to="home" smooth={true} duration={500}>
-          Home
-        </Link></li>
-        <li className="py-6 text-4xl"><Link  onClick={handleClick} to="about" smooth={true} duration={500}>
-          About
-        </Link></li>
-        <li className="py-6 text-4xl"><Link  onClick={handleClick} to="skills" smooth={true} duration={500}>
-          Skills
-        </Link></li>
-        <li className="py-6 text-4xl"><Link onClick={handleClick} to="work" smooth={true} duration={500}>
-          Work
-        </Link></li>
-        <li className="py-6 text-4xl"><Link  onClick={handleClick} to="contact" smooth={true} duration={500}>
-          Contact
-        </Link></li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
       </ul>
 
       {/* social icons */}
-      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
         <ul>
-          <li className='w-[160px]  ml-[-100px] hover:ml-[-10px] duration-300'><a className='flex justify-between items-center w-full text-gray-300 bg-blue-600' href="">LinkedIn<FaLinkedin size={30} /></a></li>
-          <li className='w-[160px]  ml-[-100px] hover:ml-[-10px] duration-300'><a className='flex justify-between items-center w-full text-gray-300 bg-[#333333]' href="">GitHub<FaGithub size={30} /></a></li>
-          <li className='w-[160px]  ml-[-100px] hover:ml-[-10px] duration-300'><a className='flex justify-between items-center w-full text-gray-300 bg-[#D0312D]' href="">Email<HiOutlineMail size={30} /></a></li>
-          <li className='w-[160px]  ml-[-100px] hover:ml-[-10px] duration-300'><a className='flex justify-between items-center w-full text-gray-300 bg-[#565F69]' href="">Resume<BsFillPersonLinesFill size={30} /></a></li>
+          <li className="w-[160px]  ml-[-100px] hover:ml-[-10px] duration-300">
+            <a
+              className="flex justify-between items-center w-full text-gray-300 bg-blue-600"
+              href="https://www.linkedin.com/in/taiwolambo/"
+            >
+              LinkedIn
+              <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="w-[160px]  ml-[-100px] hover:ml-[-10px] duration-300">
+            <a
+              className="flex justify-between items-center w-full text-gray-300 bg-[#333333]"
+              href="https://github.com/TaiwoBytes/"
+            >
+              GitHub
+              <FaGithub size={30} />
+            </a>
+          </li>
+
+          <li className="w-[160px]  ml-[-100px] hover:ml-[-10px] duration-300">
+            <a
+              className="flex justify-between items-center w-full text-gray-300 bg-[#565F69]"
+              href="https://www.linkedin.com/resume-builder/urn:li:fsd_memberResume:47255617/"
+            >
+              Resume
+              <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
         </ul>
       </div>
     </div>
